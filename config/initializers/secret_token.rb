8d4e11b9fffe57d4d8dae2d825b1cdccb4696524
Jordan Dash <jordan.dash92@gmail.com>
2013-11-07 13:06:55 -0600

    Initial commit of a default rails app. Soon to be twtitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'cb302292b177e7e623733e4cf96baa793927b8a002196cc419b4e90301a1df01535cb5d83abe073fb58925c6564f82e60c4651ae85101e43970bf685cdc00ad2'
